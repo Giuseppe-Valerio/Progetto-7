39178e61a91a4a1f8287b073fc332a87dd2d3887
Giuseppe Valerio <giuseppevalerio8989@gmail.com>
2025-01-24 15:15:04 +0100

    b

--- script.js
+++ script.js
@@ -12,9 +12,32 @@ fetch("https://striveschool-api.herokuapp.com/api/product/67936bffb7470100158b2b
       throw new Error("Request failed with status " + resp.status);
     }
   })
-  .then((prodotti) => {
-    console.log(prodotti);
+  .then((prodotto) => {
+    console.log(prodotto);
+    const row = document.getElementById("product-list");
+    const li = document.createElement("li");
+    li.classList.add("list-group-item");
+
+    li.innerHTML = `
+      <strong>${prodotto.name}</strong>
+      <p>${prodotto.description}</p>
+      <p><strong>€${prodotto.price}</strong></p>
+      <img src="${prodotto.imageUrl}" alt="${prodotto.name}" style="width: 100px; height: 100px;" />
+    `;
+
+    row.appendChild(li);
   })
   .catch((error) => {
     console.error("Error:", error);
   });
+
+/* appointments.forEach(app => {
+    console.log(app);
+
+    const li = document.createElement("li");
+    li.classList.add("list-group-item", "d-flex", "align-items-center");
+    // alla fine di questa stringa stiamo determinando la creazione di un link DINAMICO che avrà dentro di sé l'informazione dell'id specifico da portare e rendere disponibile nella pagina dettaglio
+    li.innerHTML = `<span class="me-auto">${app.name}</span> <span class="badge text-bg-dark me-2">${app.price}€</span><a href="./details.html?appId=${app._id}">VAI A DETTAGLIO</a>`;
+
+    row.appendChild(li);
+  }); */
